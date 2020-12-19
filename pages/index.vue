4c88fe0aba7a91921672122a3ebf686ac3839235
Justin Swaney <jswaney@mit.edu>
2020-12-19 01:10:53 -0500

    fix: remove registered components

--- pages/index.vue
+++ pages/index.vue
@@ -40,15 +40,3 @@
     </v-col>
   </v-row>
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
-}
-</script>
