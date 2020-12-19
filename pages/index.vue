d393fcb5b4dc0e659da764bf1723fea55510e1c3
Justin Swaney <jswaney@mit.edu>
2020-12-19 01:16:40 -0500

    fix: setup linter and fix formatting

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card>
-        <v-card-title class="headline">
-          Welcome!
-        </v-card-title>
+        <v-card-title class="headline"> Welcome! </v-card-title>
         <v-card-text>
           <p>
             Vuetify is a progressive Material Design component framework for
