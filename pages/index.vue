9446ba5fca95310b725fc7f2f7f4daa87b9f975b
Justin Swaney <jswaney@mit.edu>
2021-07-10 01:15:16 -0400

    feat: remove tw classes and refactor profile

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <v-container justify="center">
     <HomeAvatar />
     <v-card flat class="pa-2 mx-auto" max-width="500px" color="grey lighten-4">
-      <nuxt-content class="tw-prose" :document="about" />
+      <nuxt-content :document="about" />
     </v-card>
   </v-container>
 </template>
---
+++ pages/profile/ExperienceEntry.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1 class="text-body-2">{{ role }}</h1>
+    <h1 class="text-body-2 font-weight-medium">
+      {{ company }}
+    </h1>
+    <h1 class="pb-3 text-body-2">{{ start }} - {{ end || 'Present' }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api'
+
+interface ExperienceEntryProps {
+  role: string
+  company: string
+  start: number
+  end?: number
+}
+
+export default defineComponent({
+  props: {
+    entry: {
+      type: Object as () => ExperienceEntryProps,
+      required: true,
+    },
+  },
+  setup(props) {
+    return {
+      role: props.entry.role,
+      company: props.entry.company,
+      start: props.entry.start,
+      end: props.entry.end,
+    }
+  },
+})
+</script>
